bdd328e52922144d6473e1d63e9d81e0f66e97e3
gituser <v2gundam@gmail.com>
2017-10-09 17:04:58 +0900

    문제 01-1 완료

--- CPP_programming/Q01-1-3/Q0113.cpp
+++ CPP_programming/Q01-1-3/Q0113.cpp
@@ -6,6 +6,15 @@
 
 int main()
 {
+	int num;
+	std::cout << "2에서 9 사이의 숫자 입력 :";
+	std::cin >> num;
+
+	for (int i = 0; i < 9; i++)
+	{
+		int temp_num = (i+1) * num;
+		std::cout << num << " x " << (i + 1) << " = " << temp_num << std::endl;
+	}
     return 0;
 }
 
---
+++ CPP_programming/Q01-1-4/Q0114.cpp
@@ -0,0 +1,22 @@
+﻿// Q0114.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+
+int main()
+{
+	int num;
+	std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+	std::cin >> num;
+	if (num < 0)
+		std::cout << "프로그램 종료" << std::endl;
+		return 1;
+	double total = 50 + (num * 0.12);
+
+	std::cout << "이번 달 급여 : " << total << std::endl;
+	return 0;
+}
+
