a1abb3d31b21ef6717dc3721e1c24fc4bfdd6795
gituser <v2gundam@gmail.com>
2017-10-10 23:19:06 +0900

    01-2문제풀이완료

---
+++ CPP_programming/005FunctionOverloading/005FunctionOverloading.cpp
@@ -0,0 +1,29 @@
+﻿// 005FunctionOverloading.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+void MyFunc(void)
+{
+	std::cout << "MyFunc(void) called" << std::endl;
+}
+
+void MyFunc(char c)
+{
+	std::cout << "MyFunc(char c) called" << std::endl;
+}
+
+void MyFunc(int a, int b)
+{
+	std::cout << "Myfunc(int a, int b) called" << std::endl;
+}
+
+int main()
+{
+	MyFunc();
+	MyFunc('A');
+	MyFunc(12, 13);
+	return 0;
+}
+
---
+++ CPP_programming/Q01-2/Q012.cpp
@@ -0,0 +1,47 @@
+﻿// Q012.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+void swap(int *num_a, int *num_b)
+{
+	int num_temp = *num_a;
+	*num_a = *num_b;
+	*num_b = num_temp;
+}
+
+void swap(char *ch_a, char *ch_b)
+{
+	char num_temp = *ch_a;
+	*ch_a = *ch_b;
+	*ch_b = num_temp;
+}
+
+void swap(double *dbl_a, double *dbl_b)
+{
+	double num_temp = *dbl_a;
+	*dbl_a = *dbl_b;
+	*dbl_b = num_temp;
+}
+
+
+
+
+int main()
+{
+	int num1 = 20, num2 = 30;
+	swap(&num1, &num2);
+	std::cout << num1 << ' ' << num2 << std::endl;
+
+	char ch1 = 'A', ch2 = 'Z';
+	swap(&ch1, &ch2);
+	std::cout << ch1 << ' ' << ch2 << std::endl;
+
+	double dbl1 = 1.111, dbl2 = 5.555;
+	swap(&dbl1, &dbl2);
+	std::cout << dbl1 << ' ' << dbl2 << std::endl;
+	return 0;
+}
+
